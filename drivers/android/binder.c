c64bfd0cd4a0cff45362a060c694c89c58d7eb7f
xibao <xibao@oneplus.net>
2017-11-20 18:35:33 +0800

    [BSP]:[17819] change oemlogkit debugfs usage 1/2
    Plan :In order to prevent sepolicy error in android N, new filenode at procfs for oemlogkit using
    Type :USER
    Test :trigger anr and check if oem_log have transaction info
    
    Change-Id: I0cc64ecc6678d809f23fed22b938e554f4d0d452
    (cherry picked from commit 1364f66aa308d489500114fcac1396b4ebd38107)
    (cherry picked from commit 6aefa595f61279fe9f863428e454435b7544104d)

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -71,6 +71,12 @@
 #include <linux/security.h>
 #include <linux/spinlock.h>
 
+// neiltsai, 20161115, add for oemlogkit used
+#ifdef VENDOR_EDIT
+#include <linux/proc_fs.h>
+#endif
+// neiltsai end
+
 #ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
 #define BINDER_IPC_32BIT 1
 #endif
@@ -5858,6 +5864,59 @@ static int __init init_binder_device(const char *name)
 	return ret;
 }
 
+// neiltsai, 20161115, add for oemlogkit used
+#ifdef VENDOR_EDIT
+static int proc_state_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, binder_state_show, NULL);
+}
+
+static int proc_transactions_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, binder_transactions_show, NULL);
+}
+
+static int proc_transaction_log_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, binder_transaction_log_show,
+		&binder_transaction_log);
+}
+
+
+static const struct file_operations proc_state_operations = {
+	.open       = proc_state_open,
+	.read       = seq_read,
+	.llseek     = seq_lseek,
+	.release    = single_release,
+};
+
+static const struct file_operations proc_transactions_operations = {
+	.open       = proc_transactions_open,
+	.read       = seq_read,
+	.llseek     = seq_lseek,
+	.release    = single_release,
+};
+
+static const struct file_operations proc_transaction_log_operations = {
+	.open       = proc_transaction_log_open,
+	.read       = seq_read,
+	.llseek     = seq_lseek,
+	.release    = single_release,
+};
+
+static int binder_proc_init(void)
+{
+	proc_create("proc_state", 0444, NULL,
+			&proc_state_operations);
+	proc_create("proc_transactions", 0444, NULL,
+			&proc_transactions_operations);
+	proc_create("proc_transaction_log", 0444, NULL,
+			&proc_transaction_log_operations);
+	return 0;
+}
+#endif
+// neiltsai end
+
 static int __init binder_init(void)
 {
 	int ret;
@@ -5922,6 +5981,11 @@ static int __init binder_init(void)
 		if (ret)
 			goto err_init_binder_device_failed;
 	}
+	// neiltsai, 20161115, add for oemlogkit used
+#ifdef VENDOR_EDIT
+		binder_proc_init();
+#endif
+	// neiltsai end
 
 	return ret;
 
