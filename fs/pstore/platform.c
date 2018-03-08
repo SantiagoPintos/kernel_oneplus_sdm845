046262e11ca1455e36e4ff94ce7ef270bf95c300
shankai <shankai@oneplus.cn>
2018-03-08 21:29:35 +0800

    [BSP]:[17819] [17819][BSP]add ramoops function,default dump will include kmsg.txt, fix build error
    Plan:no
    Type:PATCH
    Test:check dump will include kmsg.txt
    *
    
    Change-Id: I03c4a47677d630fabe5f82645df026708c63e5dc

--- fs/pstore/platform.c
+++ fs/pstore/platform.c
@@ -592,6 +592,39 @@ static void pstore_console_write(struct console *con, const char *s, unsigned c)
 	}
 }
 
+#ifdef VENDOR_EDIT /*pstore memset befor use*/
+static void  pstore_console_init(void )
+{
+    size_t oldsize;
+    size_t size =0;
+    unsigned long flags;
+    u64 id;
+    struct ramoops_context *cxt = psinfo->data;
+
+    size = cxt->console_size;
+
+    oldsize = psinfo->bufsize;
+
+    if (psinfo == NULL)
+        return;
+
+    if (size > psinfo->bufsize)
+        size = psinfo->bufsize;
+
+    if (oops_in_progress) {
+        if (!spin_trylock_irqsave(&psinfo->buf_lock, flags))
+            return;
+    } else {
+        spin_lock_irqsave(&psinfo->buf_lock, flags);
+    }
+    memset(psinfo->buf, ' ', size);
+    psinfo->write(PSTORE_TYPE_CONSOLE, 0, &id, 0, 0, 0, size, psinfo);
+    spin_unlock_irqrestore(&psinfo->buf_lock, flags);
+
+    psinfo->bufsize = oldsize ;
+}
+#endif
+
 static struct console pstore_console = {
 	.name	= "pstore",
 	.write	= pstore_console_write,
@@ -601,6 +634,9 @@ static struct console pstore_console = {
 
 static void pstore_register_console(void)
 {
+#ifdef VENDOR_EDIT /*pstore memset befor use*/
+    pstore_console_init();
+#endif
 	register_console(&pstore_console);
 }
 
